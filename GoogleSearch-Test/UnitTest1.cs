d6eec855f28c23ca9725653102c68d926b806021
Mateus Filipe <mateusfilipe557@gmail.com>
2021-10-09 20:06:30 -0300

    Adicionando testes para pesquisa, botão de pesquisa e botão estou com sorte

--- GoogleSearch-Test/UnitTest1.cs
+++ GoogleSearch-Test/UnitTest1.cs
@@ -1,7 +1,10 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
 
 namespace GoogleSearch_Test
 {
@@ -18,11 +21,38 @@ namespace GoogleSearch_Test
         }
 
         [Test]
-        public void Test1()
+        public void BarraDePesquisaTest()
         {
             Driver.Navigate().GoToUrl("https://google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("CONCERT Technologies");
+            Driver.FindElement(By.Name("q")).Submit();
 
-            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PesquisaBtnTest()
+        {
+            Driver.Navigate().GoToUrl("https://google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("CONCERT Technologies");
+            IWebElement PesquisaBtn = Driver.FindElement(By.Name("btnK"));
+            System.Threading.Thread.Sleep(2000);
+            PesquisaBtn.Click();
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void EstouComSorteBtnTest()
+        {
+            Driver.Navigate().GoToUrl("https://google.com");
+            //Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            //Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.Name("q")).SendKeys("CONCERT Technologies");
+            IWebElement EstouComSorteBtn = Driver.FindElement(By.Name("btnI"));
+            System.Threading.Thread.Sleep(2000);
+            EstouComSorteBtn.Click();
+
             Assert.Pass();
         }
     }
